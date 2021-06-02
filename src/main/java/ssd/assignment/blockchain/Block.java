3d4775c73a646297076ee627316452a428fad358
Pedro Jorge <pedmjjorge@gmail.com>
2021-06-02 18:05:48 +0100

    Added block header

--- src/main/java/ssd/assignment/blockchain/Block.java
+++ src/main/java/ssd/assignment/blockchain/Block.java
@@ -6,64 +6,53 @@ import static ssd.assignment.config.Constraints.MINING_DIFFICULTY;
 
 public class Block{
 
-    private static Crypto crypto = new Crypto();
+    private static final Crypto crypto = new Crypto();
 
-    private int index;
-    private long timestamp;
-    private int difficulty;
-    private int nonce = 0;
+    private BlockHeader header;
     private String transactions;
-    private String hash;
-    private String parentHash;
 
     public Block() {
-        this.index = 0;
-        this.difficulty = MINING_DIFFICULTY;
+        this.header = new BlockHeader();
         this.transactions = "";
-        this.parentHash = "0";
-        this.hash = crypto.sha256(this.toString());
     }
 
     public Block (int index, String transactions, String parentHash) {
-        this.index = index;
-        this.difficulty = MINING_DIFFICULTY;
+        this.header = new BlockHeader(index, parentHash);
         this.transactions = transactions;
-        this.parentHash = parentHash;
-        this.hash = crypto.sha256(this.toString());
     }
 
     public String mineBlock() {
-        String prefix = new String(new char[this.difficulty]).replace('\0', '0');
-        this.timestamp = System.currentTimeMillis();
-        while(!hash.substring(0, this.difficulty).equals(prefix)) {
-            nonce++;
-            hash = crypto.sha256(this.toString());
+        String prefix = new String(new char[this.header.difficulty]).replace('\0', '0');
+        this.header.timestamp = System.currentTimeMillis();
+        while(!this.header.hash.substring(0, this.header.difficulty).equals(prefix)) {
+            this.header.nonce ++;
+            this.header.hash = crypto.sha256(this.toString());
         }
-        return hash;
+        return this.header.hash;
     }
 
     public int getIndex() {
-        return index;
+        return this.header.index;
     }
 
     public void setIndex(int index) {
-        this.index = index;
+        this.header.index = index;
     }
 
     public long getTimestamp() {
-        return this.timestamp;
+        return this.header.timestamp;
     }
 
     public int getDifficulty() {
-        return this.difficulty;
+        return this.header.difficulty;
     }
 
     public int getNonce() {
-        return this.nonce;
+        return this.header.nonce;
     }
 
     public void setNonce(int nonce) {
-        this.nonce = nonce;
+        this.header.nonce = nonce;
     }
 
     public String getTransactions() {
@@ -75,21 +64,50 @@ public class Block{
     }
 
     public String getHash() {
-        return hash;
+        return this.header.hash;
     }
     public void setHash(String hash) {
-        this.hash = hash;
+        this.header.hash = hash;
     }
     public String getParentHash() {
-        return parentHash;
+        return this.header.parentHash;
     }
 
     public void setParentHash(String parentHash) {
-        this.parentHash = parentHash;
+        this.header.parentHash = parentHash;
     }
 
     @Override
     public String toString() {
-        return this.index + this.timestamp + this.difficulty + this.nonce + this.transactions + this.parentHash;
+        return this.header.toString() + this.transactions;
+    }
+
+    public static class BlockHeader {
+
+        private int index;
+        private long timestamp;
+        private int difficulty;
+        private int nonce = 0;
+        private String hash;
+        private String parentHash;
+
+        public BlockHeader() {
+            this.index = 0;
+            this.difficulty = MINING_DIFFICULTY;
+            this.parentHash = "0";
+            this.hash = crypto.sha256(this.toString());
+        }
+
+        public BlockHeader(int index, String parentHash) {
+            this.index = index;
+            this.difficulty = MINING_DIFFICULTY;
+            this.parentHash = parentHash;
+            this.hash = crypto.sha256(this.toString());
+        }
+
+        public String toString() {
+            return this.index + this.timestamp + this.difficulty + this.nonce + this.parentHash;
+        }
+
     }
 }
